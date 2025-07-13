b05d79a4f968ee31499b0c7b4cb95eddf89a6061
subbni <slfkak@naver.com>
2025-07-13 14:01:47 +0900

    [refactor] Drawer 컴포넌트화

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:habit_tracker/theme/theme_provider.dart';
-import 'package:provider/provider.dart';
+import 'package:habit_tracker/components/my_drawer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,20 +11,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      drawer: Drawer(
-        backgroundColor: Theme.of(context).colorScheme.surface,
-        child: Center(
-          child: CupertinoSwitch(
-            value: Provider.of<ThemeProvider>(context).isDarkMode,
-            onChanged: (value) => Provider.of<ThemeProvider>(
-              context,
-              listen: false,
-            ).toggleTheme(),
-          ),
-        ),
-      ),
-    );
+    return Scaffold(appBar: AppBar(), drawer: const MyDrawer());
   }
 }
