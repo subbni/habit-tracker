86f8f455b9dca573cf742a7a22bf14f14f05f8ef
subbni <slfkak@naver.com>
2025-07-13 13:06:54 +0900

    [init] 프로젝트 생성 및 기본 세팅

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:habit_tracker/pages/home_page.dart';
+import 'package:habit_tracker/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MainApp(),
+    ),
+  );
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: Provider.of<ThemeProvider>(context).themeData,
+    );
+  }
+}
